fc1e0aefabba31b65f47e4257bd0a73ab2ea0d52
PhilippBuschhaus <buschhaus00@gmail.com>
2023-07-31 01:26:44 +0100

    make it work

--- src/index.js
+++ src/index.js
@@ -1,46 +1,110 @@
 import { TCString } from "@iabtcf/core";
+import accordion from "./templates/accordion.njk";
+import tabledata from "./templates/tabledata.njk";
+import results from "./templates/results.njk";
 
 function getConsentString() {
-  const consentStringInput =
-    document.getElementById("consentstringinput").value;
-
+  const consentStringInput = document
+    .getElementById("consentstringinput")
+    .value.trim();
   try {
     const decodedString = TCString.decode(consentStringInput);
-    console.log(decodedString);
-
-    for (let key in decodedString) {
-      if (decodedString.hasOwnProperty(key)) {
-        addResult(key, decodedString[key]);
-      }
-    }
+    processConsentString(decodedString);
   } catch (error) {
+    console.log(error);
     alert("Invalid consent string");
     return;
   }
 }
 
+function processConsentString(decodedString) {
+  console.log(decodedString);
+  addResultHtmlWrapper();
+
+  for (let key in decodedString) {
+    if (decodedString.hasOwnProperty(key)) {
+      if (!shouldShowToUser(key)) {
+        continue;
+      }
+
+      if (
+        typeof decodedString[key] !== "object" ||
+        key == "created" ||
+        key == "lastUpdated"
+      ) {
+        addResult(key, decodedString[key]);
+      } else if (typeof decodedString[key] == "object") {
+        addListResult(key, decodedString[key]);
+      }
+    }
+  }
+}
+
+function addResultHtmlWrapper() {
+  var resultsHtmlWrapper = results();
+
+  document.getElementById("result").innerHTML = resultsHtmlWrapper;
+}
+
 function addResult(key, value) {
-  const resultDiv = document.getElementById("result");
+  console.log(key);
 
-  const ulElement = document.createElement("ul");
-  ulElement.classList.add("list-group", "mb-3", "list-group-horizontal");
+  var tableDataHtml = tabledata({
+    key: key,
+    value: value,
+  });
 
-  const liElementKey = document.createElement("li");
-  liElementKey.classList.add("list-group-item");
+  var nodeToInsert = document.createElement("tr");
+  nodeToInsert.classList.add("mb-3");
+  nodeToInsert.innerHTML = tableDataHtml;
 
-  const textNodeKey = document.createTextNode(key);
-  liElementKey.appendChild(textNodeKey);
+  document.getElementById("tableresult").appendChild(nodeToInsert);
+}
 
-  const liElementValue = document.createElement("li");
-  liElementValue.classList.add("list-group-item");
+function shouldShowToUser(name) {
+  const namesToShow = {
+    specialFeatureOptins: true,
+    purposeConsents: true,
+    purposeLegitimateInterests: true,
+    vendorConsents: true,
+    vendorLegitimateInterests: true,
+    created: true,
+    lastUpdated: true,
+    cmpId_: true,
+    cmpVersion_: true,
+    consentScreen_: true,
+    consentLanguage_: true,
+    vendorListVersion_: true,
+    policyVersion_: true,
+    isServiceSpecific_: true,
+    useNonStandardStacks_: true,
+    purposeOneTreatment_: true,
+    publisherCountryCode_: true,
+  };
 
-  const textNodeValue = document.createTextNode(value);
-  liElementValue.appendChild(textNodeValue);
+  return namesToShow.hasOwnProperty(name);
+}
 
-  ulElement.appendChild(liElementKey);
-  ulElement.appendChild(liElementValue);
+function addListResult(key, obj) {
+  var tableDataHtml = "";
+  obj.forEach((v, i) => {
+    tableDataHtml += tabledata({
+      key: i,
+      value: v,
+    });
+  });
 
-  resultDiv.insertAdjacentElement("afterend", ulElement);
+  var fullAccHtml = accordion({
+    tabledata: tableDataHtml,
+    category: key,
+    id: "Collapse" + key,
+  });
+
+  const accordionInDom = document.getElementById("dataAccordion");
+
+  var nodeToInsert = document.createElement("div");
+  nodeToInsert.innerHTML = fullAccHtml;
+  accordionInDom.appendChild(nodeToInsert);
 }
 
 const submitButton = document.getElementById("consentstringsubmit");
--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,64 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const webpack = require("webpack");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name][contenthash].js",
-    clean: true,
-  },
-  devtool: "source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "src"),
+module.exports = (_, argv) => {
+  const mode = argv.mode || "development";
+
+  return {
+    mode: mode,
+    entry: {
+      bundle: path.resolve(__dirname, "src/index.js"),
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name][contenthash].js",
+      clean: true,
+    },
+    devtool: "source-map",
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "src"),
+      },
+      port: 8384,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
+        {
+          test: /\.njk$/,
+          use: [
+            {
+              loader: "simple-nunjucks-loader",
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Consent String Decoder",
+        filename: "index.html",
+        template: "src/template.html",
+      }),
+
+      // new webpack.HotModuleReplacementPlugin(),
+      // new BundleAnalyzerPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Consent String Decoder",
-      filename: "index.html",
-      template: "src/template.html",
-    }),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new BundleAnalyzerPlugin(),
-  ],
-  //   watch: true, // Enable watch mode
+    //   watch: true, // Enable watch mode
+  };
 };
